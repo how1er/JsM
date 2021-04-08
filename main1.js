c3167a773861dd14e545cfb65792d8fb2728864c
how1er <azamatkfu@gmail.com>
2021-04-08 09:53:28 +0300

    добавил функцию createPlayer в main1.js

--- main1.js
+++ main1.js
@@ -16,6 +16,30 @@ const player1={
     img:' ',
     weapon:['меч'],
     attack:function(){
-        console.log(player1.name+'fight..')
+        console.log(player1.name+' fight..')
     },
 }
+
+function createPlayer(){
+    const $player=document.createElement('div');
+    $player.classList.add('player');
+    const $progressbar =document.createElement('div');
+    $progressbar.classList.add('progressbar');
+    const $life =document.createElement('div');
+    $life.style.width='100%';
+    $life.classList.add('life');
+    const $name =document.createElement('div');
+    $name.classList.add('name');
+    $name.innerText='scorpion';
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+    $player.appendChild($progressbar);
+    const $character=document.createElement('div');
+    const $img=document.createElement('img');
+    $img.src='http://reactmarathon-api.herokuapp.com/assets/liukang.gif';
+    $character.appendChild($img);
+    $player.appendChild($character);
+    document.body.appendChild($player);
+}
+createPlayer();
+    
