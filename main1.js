adb663976b22b11d3520e2e609fe71a8827375cb
how1er <azamatkfu@gmail.com>
2021-04-08 08:11:02 +0300

    создал новый объект в файле main1.js

---
+++ main1.js
@@ -0,0 +1,21 @@
+// const box=document.getElementById('box')
+
+// console.log(box.innerText)
+
+// box.style.width='50px';
+
+// box.style.background='green';
+
+// box.addEventListener('click',function(){
+//     alert('BUMM')
+// })
+
+const player1={
+    name:'scorpion',
+    hp:11,
+    img:' ',
+    weapon:['меч'],
+    attack:function(){
+        console.log(player1.name+'fight..')
+    },
+}
